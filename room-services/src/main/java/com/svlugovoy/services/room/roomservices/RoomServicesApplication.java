45c87c02ca7016eb25473c684817ea89d06e8f8f
svlugovoy <svlugovoy@gmail.com>
2018-09-10 13:02:39 +0300

    Started developing first service

---
+++ room-services/src/main/java/com/svlugovoy/services/room/roomservices/RoomServicesApplication.java
@@ -0,0 +1,12 @@
+package com.svlugovoy.services.room.roomservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomServicesApplication.class, args);
+    }
+}
