79fd0d96fe176752cd5bd912b465305394f717ce
svlugovoy <svlugovoy@gmail.com>
2018-09-10 13:49:03 +0300

    Added swagger support for room-services

--- room-services/src/main/java/com/svlugovoy/services/room/roomservices/RoomController.java
+++ room-services/src/main/java/com/svlugovoy/services/room/roomservices/RoomController.java
@@ -1,5 +1,7 @@
 package com.svlugovoy.services.room.roomservices;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/rooms")
+@Api(value="rooms", description = "Data service operations on rooms", tags=("rooms"))
 public class RoomController {
 
     private RoomRepository roomRepository;
@@ -20,6 +23,7 @@ public class RoomController {
     }
 
     @GetMapping
+    @ApiOperation(value="Get All Rooms", notes="Gets all rooms in the system", nickname="getRooms")
     public List<Room> findAll(@RequestParam(name = "roomNumber", required = false) String roomNumber) {
         if (StringUtils.isNotEmpty(roomNumber)) {
             return Collections.singletonList(this.roomRepository.findByRoomNumber(roomNumber));
--- room-services/src/main/java/com/svlugovoy/services/room/roomservices/RoomServicesApplication.java
+++ room-services/src/main/java/com/svlugovoy/services/room/roomservices/RoomServicesApplication.java
@@ -2,9 +2,28 @@ package com.svlugovoy.services.room.roomservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.any;
 
 @SpringBootApplication
+@EnableSwagger2
 public class RoomServicesApplication {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).groupName("Room").select()
+                .apis(RequestHandlerSelectors.basePackage("com.svlugovoy.services.room.roomservices"))
+                .paths(any()).build().apiInfo(new ApiInfo("Room Services",
+                        "A set of services to provide data access to rooms", "1.0.0", null,
+                        new Contact("Serhii Luhovyi", "https://svlugovoy.com", null), null, null));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(RoomServicesApplication.class, args);
